5d4198acd2202881aa9f663cb2eb2c77847197ef
Victor Vu <79396055+victorvuthanh@users.noreply.github.com>
2021-07-02 00:18:39 +0300

    Update tables.sql
    + functions

--- curs/tables.sql
+++ curs/tables.sql
@@ -432,3 +432,75 @@ CREATE TABLE follow_list (
 		ON UPDATE CASCADE
 );
 
+
+-- Функции
+
+
+-- счет голосов по жандрам
+DROP FUNCTION IF EXISTS g_relevancy;
+DELIMITER //
+CREATE FUNCTION g_relevancy(t_id INT UNSIGNED, g_id INT UNSIGNED)
+	RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN (SELECT likes.c - dislikes.c
+			  FROM (
+					   SELECT count(vote) AS c
+						 FROM votes_on_genre
+						WHERE vote = 1 AND title_id = t_id AND genre_id = g_id
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_genre
+				   WHERE vote = 0 AND title_id = t_id AND genre_id = g_id
+							) AS dislikes
+		   );
+END; //
+DELIMITER ;
+
+
+
+-- счет голосов по ключевым словам
+DROP FUNCTION IF EXISTS k_relevancy;
+DELIMITER //
+CREATE FUNCTION k_relevancy(t_id INT UNSIGNED, k_id INT UNSIGNED)
+	RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN (SELECT likes.c - dislikes.c
+			  FROM (
+					   SELECT count(vote) AS c
+						 FROM votes_on_keywords
+						WHERE vote = 1 AND title_id = t_id AND keyword_id = k_id
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_keywords
+				   WHERE vote = 0 AND title_id = t_id AND keyword_id = k_id
+							) AS dislikes
+		   );
+END;
+//
+DELIMITER ;
+
+
+
+-- Смотреть рейтинг
+DROP FUNCTION IF EXISTS review_rate;
+DELIMITER //
+CREATE FUNCTION review_rate(r_id INT UNSIGNED)
+	RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN (SELECT likes.c - dislikes.c
+			  FROM (
+					   SELECT count(vote) AS c
+						 FROM votes_on_reviews
+						WHERE vote = 1 AND review_id = r_id
+				   ) AS likes
+					   JOIN (
+				  SELECT count(vote) AS c
+					FROM votes_on_reviews
+				   WHERE vote = 0 AND review_id = r_id
+							) AS dislikes
+		   );
+END;
+//
+DELIMITER ;
